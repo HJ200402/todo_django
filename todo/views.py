4c7c6602fc7b9ca44aabdf9b0e7f865aae4abdbe
Hong <xcalibur@naver.com>
2020-04-28 11:30:55 +0900

    form_추가

--- todo/views.py
+++ todo/views.py
@@ -15,8 +15,9 @@ def todo_view(request):
         "todos" : todos,
         "form" : form,
     }
-
+    
     return render(request, "todo_list.html", data)
+
 
 def todo_progress_view(request):
     todos = Todo.objects.exclude(is_done=True)
