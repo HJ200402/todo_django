891b7f8c64b35b18281243a4af2b63cec589af3e
Hong <xcalibur@naver.com>
2020-05-05 16:25:46 +0900

    settings.py 수정

--- coffee/settings.py
+++ coffee/settings.py
@@ -121,5 +121,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+    '/home/static/',
+    '/todo/static/',
 )
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
